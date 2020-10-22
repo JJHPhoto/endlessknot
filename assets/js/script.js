1647cc24348d5d87ac379f42fdc2efc14bbdd4fa
Joshua J. Haller <phone@joshuajhaller.com>
2020-10-22 16:56:00 -0700

    more pseudocode for js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//What do I need? 
+// var of Numbers
+// var of LowercCase
+// var of UpperCase
+// var of ZpecialCharacters
+
 // Write password to the #password input
 function writePassword() {
   //PROMPT about criteria needed? 
-  //PROMPT length of 'passwordrequired'. 8 <= %% => 128.
-  //PROMPT if lowerCase, upperCase, number, or specialCharacters are wanted.
+  //PROMPT length of 'passwordRequires'. 8 <= %% => 128.
+  //PROMPT if LowerCase, UpperCase, Number, or SpecialCharacters are wanted.
   //WRITE characterTypes to password. 
-  //generatePassword to the page. 
+  //generatePassword to the page.
+
+  //if 'false' ( ! (passwordRequires >= 8 || passwordRequires <= 128) ) PROMPT user with "Please pick a longer/shorter password."
+
+
+  //Need to WRITE variables to 'password' when selected
+
+  //if 'usedNumbers' 
+  //then .... write? how?
+
+
+  //if 'usedLowerCase' 
+  //then .... ""
+
+  //if 'usedUpperCase' 
+  //then .... ""
+  
+  //if 'usedSpecialCharacters' 
+  //then .... ""
+
+
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -20,4 +46,4 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-// I think I need to be able to have a "tryagainbutton" if customer is not satisfied
+// I think I need to be able to have a "TryAgainbutton" if the user is not satisfied
