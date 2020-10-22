6004bfb7a639f9714c74ffb694e6a26ae94484c1
Joshua J. Haller <phone@joshuajhaller.com>
2020-10-22 16:45:31 -0700

    Wrote my pseudocode for js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,12 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  //PROMPT about criteria needed? 
+  //PROMPT length of 'passwordrequired'. 8 <= %% => 128.
+  //PROMPT if lowerCase, upperCase, number, or specialCharacters are wanted.
+  //WRITE characterTypes to password. 
+  //generatePassword to the page. 
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -12,3 +18,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// I think I need to be able to have a "tryagainbutton" if customer is not satisfied
