f886ef0c44a208668601dd2372434597dfee946f
Joshua J. Haller <phone@joshuajhaller.com>
2020-10-27 09:54:51 -0700

    Trying to create functions to go into my generatePassword function.

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # The Endlessknot
 
-### 1. 
+### 1. Begining
 This homework assignment was to get a password generator to work. We were given a working HTML page styled with CSS and some JS functions. 
 
 My first course of action was to read through the README & try to pseudocode what I would need to get the password generator to work. I then started to look up YouTube tutorials that would help me figure things out. I have listed the two I found most useful in my resources. I knew they were doing much more complex work than I was, but watching them process and connect things helped a lot. I also read some articles on Sitepoint but most were too vague for me to be helpful. 
+
+### 2. Finishing Up
+After running into a few issues in my function to generate a password, I took the advice of a TA and decided to break up my one, big "generatePassword" function into a few functions. This was then demo'd by our class instructor, which encouraged me that I was on the right path. 
 
 
 ### 3. Resources
--- assets/js/script.js
+++ assets/js/script.js
@@ -24,51 +24,115 @@ wordbank += uppercase;
 wordbank += symbols;
 // console.log (wordbank);
 
-function generatePassword() { 
-    //PROMPT for criteria (password length)
+//User input for password length.
+function passwordLength() {
+      //PROMPT for criteria (password length)
     var pwdLength = parseInt(prompt("How long do you want your password to be? (Needs to be between 8-128 to be secure.)") );
 
-    //PROMPT If the user doesn't pick a password long enough. 
-    if (pwdLength < 8 || pwdLength > 128); {
-      alert ("Please pick a password with a length of 8-128.")
-      // console.log (pwdLength);
-      //Not accepting any "correct" numbers, but it is in the console.log Maybe need to build out more. 
-      // return;
-    } 
-  
-    //CONFIRM if LowerCase, UpperCase, Number, or SpecialCharacters are wanted.
-    var pwdNumbers = confirm ("Would you like to include numbers?");
-    if (pwdNumbers === true) {
-      wordbank = wordbank.concat(numbers); 
-      // console.log (getnumbers);
+    // //PROMPT If the user doesn't pick a password long enough. 
+    if (pwdLength < 8 || pwdLength > 128) {
+      alert ("Please pick a password with a length between 8-128.")
+      console.log(pwdLength);
     }
-  
-    var pwdLowerCase = confirm ("How about lower case letters?");
-    if (pwdLowerCase === true) {
-      wordbank = wordbank.concat(lowercase); 
-      // console.log (getlowercase);
-    }
-  
-    var pwdUpperCase = confirm ("Maybe some upper case letters?");
-    if (pwdUpperCase === true) {
-      wordbank = wordbank.concat(uppercase);
-      // console.log (getuppercase); 
-    }
-  
-    var pwdSymbols = confirm ("And to finish it off, how about some symbols?");
-    if (pwdSymbols === true) {
-      wordbank = wordbank.concat(symbols);
-      // console.log (getsymbols); 
-    }
-  
-    for (var i = 0; i < pwdLength; i++) {
-      var pulledCharacters = wordbank[Math.floor(Math.random() * wordbank.length)];
-      password += pulledCharacters;
-    }
-    console.log (pulledCharacters)
+
+    return pwdLength;
+}
+
+function charactersPicked () {
+
+      var selections = "";
+
+      //CONFIRM if LowerCase, UpperCase, Number, or SpecialCharacters are wanted.
+      var pwdNumbers = confirm ("Would you like to include numbers?");
+      if (pwdNumbers === true) {
+        wordbank = wordbank.concat(numbers); 
+        console.log (getnumbers);
+      }
+    
+      var pwdLowerCase = confirm ("How about lower case letters?");
+      if (pwdLowerCase === true) {
+        wordbank = wordbank.concat(lowercase); 
+        console.log (getlowercase);
+      }
+    
+      var pwdUpperCase = confirm ("Maybe some upper case letters?");
+      if (pwdUpperCase === true) {
+        wordbank = wordbank.concat(uppercase);
+        console.log (getuppercase); 
+      }
+    
+      var pwdSymbols = confirm ("And to finish it off, how about some symbols?");
+      if (pwdSymbols === true) {
+        wordbank = wordbank.concat(symbols);
+        console.log (getsymbols); 
+      }
+
+      return selections;
+}
+
+//Password length function.
+// function passwordLength() {
+
+//   for (var i = 0; i < pwdLength; i++) {
+//     var pulledCharacters = wordbank[Math.floor(Math.random() * wordbank.length)];
+//     password += pulledCharacters;
+//     console.log (pulledCharacters)
+//   }
+// }
+
+//Password generate function.
+function generatePassword() { 
+
+
+
     //pulling the correct number, just in a object block. 
     return password;
 
+    //PROMPT for criteria (password length)
+    // var pwdLength = parseInt(prompt("How long do you want your password to be? (Needs to be between 8-128 to be secure.)") );
+
+    // //PROMPT If the user doesn't pick a password long enough. 
+    // if (pwdLength < 8 || pwdLength > 128) {
+    //   alert ("Please pick a password with a length of 8-128.")
+      // console.log (pwdLength);
+      //Not accepting any "correct" numbers, but it is in the console.log Maybe need to build out more. 
+      // break;
+    // } 
+    // else {
+    //   return;
+    // }
+
+    // //CONFIRM if LowerCase, UpperCase, Number, or SpecialCharacters are wanted.
+    // var pwdNumbers = confirm ("Would you like to include numbers?");
+    // if (pwdNumbers === true) {
+    //   wordbank = wordbank.concat(numbers); 
+    //   // console.log (getnumbers);
+    // }
+  
+    // var pwdLowerCase = confirm ("How about lower case letters?");
+    // if (pwdLowerCase === true) {
+    //   wordbank = wordbank.concat(lowercase); 
+    //   // console.log (getlowercase);
+    // }
+  
+    // var pwdUpperCase = confirm ("Maybe some upper case letters?");
+    // if (pwdUpperCase === true) {
+    //   wordbank = wordbank.concat(uppercase);
+    //   // console.log (getuppercase); 
+    // }
+  
+    // var pwdSymbols = confirm ("And to finish it off, how about some symbols?");
+    // if (pwdSymbols === true) {
+    //   wordbank = wordbank.concat(symbols);
+    //   // console.log (getsymbols); 
+    // }
+
+  
+    // for (var i = 0; i < pwdLength; i++) {
+    //   var pulledCharacters = wordbank[Math.floor(Math.random() * wordbank.length)];
+    //   password += pulledCharacters;
+    // }
+    // console.log (pulledCharacters)
 
 }
 
