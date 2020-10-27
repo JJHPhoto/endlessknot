1f035cc286cb78e331ed080156f258c32047bfe5
Joshua J. Haller <phone@joshuajhaller.com>
2020-10-27 11:03:29 -0700

    Still trying to find where my error is.

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,113 +26,58 @@ wordbank += symbols;
 
 //User input for password length.
 function passwordLength() {
-      //PROMPT for criteria (password length)
+      //PROMPT for password length
     var pwdLength = parseInt(prompt("How long do you want your password to be? (Needs to be between 8-128 to be secure.)") );
+    console.log(pwdLength);
 
-    // //PROMPT If the user doesn't pick a password long enough. 
+    // //ALERT if the user doesn't pick a password long enough. 
     if (pwdLength < 8 || pwdLength > 128) {
       alert ("Please pick a password with a length between 8-128.")
-      console.log(pwdLength);
     }
-
-    return pwdLength;
 }
 
 function charactersPicked () {
 
-      var selections = "";
-
-      //CONFIRM if LowerCase, UpperCase, Number, or SpecialCharacters are wanted.
-      var pwdNumbers = confirm ("Would you like to include numbers?");
-      if (pwdNumbers === true) {
+    //CONFIRM if LowerCase, UpperCase, Number, or SpecialCharacters are wanted.
+    var pwdNumbers = confirm ("Would you like to include numbers?");
+    if (pwdNumbers === true) {
         wordbank = wordbank.concat(numbers); 
-        console.log (getnumbers);
+        console.log (numbers);
       }
     
-      var pwdLowerCase = confirm ("How about lower case letters?");
+    var pwdLowerCase = confirm ("How about lower case letters?");
       if (pwdLowerCase === true) {
         wordbank = wordbank.concat(lowercase); 
-        console.log (getlowercase);
+        console.log (lowercase);
       }
     
-      var pwdUpperCase = confirm ("Maybe some upper case letters?");
-      if (pwdUpperCase === true) {
+    var pwdUpperCase = confirm ("Maybe some upper case letters?");
+    if (pwdUpperCase === true) {
         wordbank = wordbank.concat(uppercase);
-        console.log (getuppercase); 
+        console.log (uppercase); 
       }
     
-      var pwdSymbols = confirm ("And to finish it off, how about some symbols?");
-      if (pwdSymbols === true) {
+    var pwdSymbols = confirm ("And to finish it off, how about some symbols?");
+    if (pwdSymbols === true) {
         wordbank = wordbank.concat(symbols);
-        console.log (getsymbols); 
+        console.log (symbols); 
       }
-
-      return selections;
-}
-
-//Password length function.
-// function passwordLength() {
-
-//   for (var i = 0; i < pwdLength; i++) {
-//     var pulledCharacters = wordbank[Math.floor(Math.random() * wordbank.length)];
-//     password += pulledCharacters;
-//     console.log (pulledCharacters)
-//   }
-// }
+  }
 
 //Password generate function.
 function generatePassword() { 
 
+  var pwdLength = passwordLength();
 
+  var selections = charactersPicked();
 
-    //pulling the correct number, just in a object block. 
-    return password;
+  for (var i = 0; i < pwdLength; i++) {
+  var pulledCharacters = selections[Math.floor(Math.random() * selections.length)];
+  password = pulledCharacters;
+  console.log (pulledCharacters)
+  }
 
-    //PROMPT for criteria (password length)
-    // var pwdLength = parseInt(prompt("How long do you want your password to be? (Needs to be between 8-128 to be secure.)") );
-
-    // //PROMPT If the user doesn't pick a password long enough. 
-    // if (pwdLength < 8 || pwdLength > 128) {
-    //   alert ("Please pick a password with a length of 8-128.")
-      // console.log (pwdLength);
-      //Not accepting any "correct" numbers, but it is in the console.log Maybe need to build out more. 
-      // break;
-    // } 
-    // else {
-    //   return;
-    // }
-
-    // //CONFIRM if LowerCase, UpperCase, Number, or SpecialCharacters are wanted.
-    // var pwdNumbers = confirm ("Would you like to include numbers?");
-    // if (pwdNumbers === true) {
-    //   wordbank = wordbank.concat(numbers); 
-    //   // console.log (getnumbers);
-    // }
-  
-    // var pwdLowerCase = confirm ("How about lower case letters?");
-    // if (pwdLowerCase === true) {
-    //   wordbank = wordbank.concat(lowercase); 
-    //   // console.log (getlowercase);
-    // }
-  
-    // var pwdUpperCase = confirm ("Maybe some upper case letters?");
-    // if (pwdUpperCase === true) {
-    //   wordbank = wordbank.concat(uppercase);
-    //   // console.log (getuppercase); 
-    // }
-  
-    // var pwdSymbols = confirm ("And to finish it off, how about some symbols?");
-    // if (pwdSymbols === true) {
-    //   wordbank = wordbank.concat(symbols);
-    //   // console.log (getsymbols); 
-    // }
-
-  
-    // for (var i = 0; i < pwdLength; i++) {
-    //   var pulledCharacters = wordbank[Math.floor(Math.random() * wordbank.length)];
-    //   password += pulledCharacters;
-    // }
-    // console.log (pulledCharacters)
+  return password; 
 
 }
 
